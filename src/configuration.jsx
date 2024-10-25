82f39cedd6da398bf6e6cd35dd7a0b7eb7b58476
Mahir Salihbasic <mahir.salihbasic@protonmail.com>
2024-10-25 16:21:48 +0200

    Added firebase, typescript and config

---
+++ src/configuration.jsx
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC7vd6duaD0sjsqm1sWDNs2w0-uvCd2FsE",
+  authDomain: "uninet-hackathon.firebaseapp.com",
+  projectId: "uninet-hackathon",
+  storageBucket: "uninet-hackathon.appspot.com",
+  messagingSenderId: "889378598770",
+  appId: "1:889378598770:web:083d49458db4dc33e277eb",
+  measurementId: "G-B7VVD92XXJ"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const firebaseAnalytics = getAnalytics(firebaseApp);
+
+export { firebaseApp, firebaseAnalytics }
